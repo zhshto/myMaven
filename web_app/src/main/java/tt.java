5ad0fc75706838c482ab3656e816f8801201beea
张世通 <768433658@qq.com>
2017-02-26 13:37:14 +0800

    Initial commit

---
+++ web_app/src/main/java/tt.java
@@ -0,0 +1,20 @@
+import com.test;
+
+/**
+ * ss
+ *
+ * @author shitong
+ * @create 2017-02-10  14:50
+ */
+
+public class tt {
+
+    public void t(){
+        test t = new test();
+        t.test();
+    }
+    public static void main(String args[]){
+        tt e = new tt();
+        e.t();
+    }
+}
---
+++ web_service/src/main/java/com/test.java
@@ -0,0 +1,14 @@
+package com;
+
+/**
+ * asas
+ *
+ * @author shitong
+ * @create 2017-02-10  14:40
+ */
+
+public class test {
+    public void test(){
+        System.out.println("121313");
+    }
+}
